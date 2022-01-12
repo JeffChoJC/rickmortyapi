8acd227d9f20326610d23613e8d56281be74bfc3
Jeffrey <chojeffrey@gmail.com>
2022-01-12 02:43:06 -0500

    remove router

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,9 @@ import React from 'react';
 
 import Landing from './components/Landing';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />}/>
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Landing />
 }
 
 export default App;
